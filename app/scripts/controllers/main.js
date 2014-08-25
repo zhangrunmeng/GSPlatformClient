ce1766cc6b6ad02e8121709cd8c9162cee50bfd0
zhangrunmeng <zhangrunmeng@gmail.com>
2014-08-25 15:31:55 +0800

    Initailize with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gsplatformClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gsplatformClientApp
+ */
+angular.module('gsplatformClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
